d7cc9e6688030f7f5bde2942d2505f9a0b52f8df
Thauzin | dev <thauanyfm3@gmail.com>
2025-05-28 14:50:02 -0300

    Profile feito

---
+++ cartas.php
@@ -0,0 +1,82 @@
+<?php
+session_start(); // para pegar o ID do usuário logado
+$id_usuario = $_SESSION['usuario']; 
+
+$servername = "localhost"; 
+$username = "vscode_user";
+$password = "senha123";
+$database = "gamelist";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+
+$sql = "SELECT *
+        FROM usuario_jogo uj
+        JOIN jogos j ON uj.id_jogo = j.nome
+        WHERE uj.id_usuario = ?
+        AND categoria = 'cartas'";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $id_usuario);
+$stmt->execute();
+$result = $stmt->get_result();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Biblioteca de Jogos</title>
+  <link href="Bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+  <script src="Bootstrap/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body class="bg-tertiary-color">
+  <nav class="navbar navbar-expand-lg bg-primary">
+    <div class="container-fluid d-flex align-items-center justify-content-between">
+      <div class="mx-auto">
+        <a class="nav-link active text-warning fw-bold" href="#">GAMELIST</a>
+      </div>
+      <div>
+        <a href="home.php"><button class="btn btn-outline-warning" type="button" id="perfil">HOME</button></a>
+      </div>
+    </div>
+  </nav>
+  <br>
+  <br>
+      <?php if (!empty($id_usuario)) : ?>
+      <h3 class="text-center"> Jogos de cartas da biblioteca de <?php echo htmlspecialchars($id_usuario);  ?> </h3>
+  <div class="container list-group list-group-horizontal">
+      <?php if ($result->num_rows > 0): ?>
+              <?php while ($row = $result->fetch_assoc()) : ?>
+        <div class="card ms-5 mt-3 " style="width: 18rem;">
+            <div class="card-body">
+            <h5 class="card-title"><?php echo htmlspecialchars($row['nome']); ?></h5>
+            <hr>
+            <h6 class="card-subtitle mb-2 text-body-secondary"></h6>       
+            <p class="card-text">Número de jogadores: <?php echo htmlspecialchars($row['numJogadores']);  ?> </p>
+            <p class="card-text">Tempo (em minutos):  <?php echo htmlspecialchars($row['tempo']); ?> </p>
+            <p class="card-text">Categoria:  <?php echo htmlspecialchars($row['categoria']); ?> </p>
+            </div>
+        </div>
+        <?php endwhile; ?>
+      <?php else: ?>
+        <p class="text-center mt-4">Você não tem jogos cadastrados.</p>
+      <?php endif; ?>
+    <?php else: ?>
+      <p class="text-center">Usuário não logado.</p>
+    <?php endif; ?>
+  </div>
+    <a href="profile.php"><button class="ms-5 text-xxl-end btn btn-outline-warning" type="button" id="perfil">VOLTAR</button></a>
+</body>
+</html>
+
+<?php
+$stmt->close();
+$conn->close();
+?>
--- profile.php
+++ profile.php
@@ -1,6 +1,6 @@
 <?php
 session_start(); // para pegar o ID do usuário logado
-$id_usuario = $_SESSION['usuario']; // Exemplo: você deve ter salvo isso ao logar
+$id_usuario = $_SESSION['usuario']; 
 
 ?>
 
@@ -27,34 +27,62 @@ $id_usuario = $_SESSION['usuario']; // Exemplo: você deve ter salvo isso ao log
     </div>
   </nav>
   <br>
-  <br>
-  <br>
   <div class="row">
     <?php if (!empty($id_usuario)) : ?>
     <div class="text-center fs-3" id="errou">Boa Tarde, <?php echo $id_usuario; ?></div>
  <?php endif; ?>
  <br>
  <br>
- <br>
  <div class="text-center text-primary fs-3">ACESSAR BIBLIOTECA </div>
  <br>
  <br>
- <br>
- 
- <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-  <input type="radio" class="btn-check" name="btnradio" id="tenho" autocomplete="off" checked>
-  <label class="btn btn-outline-primary" for="tenho">TENHO</label>
-
-  <input type="radio" class="btn-check" name="btnradio" id="desejado" autocomplete="off">
-  <label class="btn btn-outline-primary" for="desejado">DESEJADO</label>
-
-  <input type="radio" class="btn-check" name="btnradio" id="jogado" autocomplete="off">
-  <label class="btn btn-outline-primary" for="jogado"> JOGADO </label>
-
-   <input type="radio" class="btn-check" name="btnradio" id="favorito" autocomplete="off">
-  <label class="btn btn-outline-primary" for="favorito">FAVORITO</label>
+<div class="card ms-5 mt-3" style="width: 18rem;">
+  <img src="images/Jogos_tenho.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Tenho</h5>
+    <p class="card-text">Esses já fazem parte da coleção. Só escolher e jogar!</p>
+    <a href="tenho.php" class="btn btn-primary">        ACESSAR   </a>
+  </div>
 </div>
-
+<div class="card ms-5 mt-3" style="width: 18rem;">
+  <img src="images/Desejado.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Desejado</h5>
+    <p class="card-text">Jogos que estão na mira, só esperando a hora de chegar!</p>
+    <a href="desejado.php" class="btn btn-primary">ACESSAR</a>
+  </div>
+</div>
+<div class="card ms-5 mt-3" style="width: 18rem;">
+  <img src="images/Favorito.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Favorito</h5>
+    <p class="card-text">Aqueles que não saem da mesa! Os queridinhos de sempre.</p>
+    <a href="favorito.php" class="btn btn-primary">ACESSAR</a>
+  </div>
+</div>
+<div class="card ms-5 mt-3" style="width: 18rem;">
+  <img src="images/Cartas.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Cartas</h5>
+    <p class="card-text">Baralho na mão e jogo na mesa. É hora de blefar e rir!</p>
+    <a href="cartas.php" class="btn btn-primary">ACESSAR</a>
+  </div>
+</div>
+<div class="card ms-5 mt-3" style="width: 18rem;">
+  <img src="images/Dois Jogadores.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Dois jogadores</h5>
+    <p class="card-text">Perfeitos pra jogar a dois. Rivalidade ou parceria, você escolhe!</p>
+    <a href="#" class="btn btn-primary">ACESSAR</a>
+  </div>
+</div>
+  <div class="card ms-5 mt-3" style="width: 18rem;">
+  <img src="images/galera.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Jogar com a galera</h5>
+    <p class="card-text">Ideal para reunir a turma! Jogos para +6 jogadores</p>
+    <a href="#" class="btn btn-primary">ACESSAR</a>
+  </div>
     
 </body>
 </html>
